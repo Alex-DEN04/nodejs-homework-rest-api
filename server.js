51431f4918636543833cf89cabade6ec98ddeb72
Oleksandr Levchuk <emihrant.ua@gmail.com>
2023-08-13 18:53:45 +0200

    env

--- server.js
+++ server.js
@@ -2,19 +2,14 @@ const app = require("./app");
 
 const mongoose = require("mongoose")
 
-const DB_HOST = "mongodb+srv://Oleksandr:B7xkY_JUcELXriK@cluster0.8shbhkt.mongodb.net/db-contacts?retryWrites=true&w=majority"
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.connect(DB_HOST)
   .then(() => {
     console.log("Database connection successful");
-    app.listen(3000);
+    app.listen(PORT);
   })
   .catch(error => {
     console.log(error.message);
     process.exit(1)
   })
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
-// B7xkY_JUcELXriK
