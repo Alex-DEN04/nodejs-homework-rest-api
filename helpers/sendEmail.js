22a368e374478be66fa4c424aa0e7bab3b740fea
Oleksandr Levchuk <emihrant.ua@gmail.com>
2023-09-10 15:01:23 +0200

    verification

---
+++ helpers/sendEmail.js
@@ -0,0 +1,14 @@
+const sgMeil = require("@sendgrid/mail");
+require("dotenv").config();
+
+const { SENDGRID_API_KEY } = process.env;
+
+sgMeil.setApiKey(SENDGRID_API_KEY);
+
+const sendEmail = async (data) => {
+    const email = { ...data, from: "emihrant.ua@gmail.com" }
+    await sgMeil.send(email)
+    return true;
+}
+
+module.exports = sendEmail;
