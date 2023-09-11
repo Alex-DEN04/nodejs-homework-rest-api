68836203d6a16412dd3566419472f97837d25b93
Oleksandr Levchuk <emihrant.ua@gmail.com>
2023-09-11 18:56:29 +0200

    moved email to env

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,12 +1,12 @@
 const sgMeil = require("@sendgrid/mail");
 require("dotenv").config();
 
-const { SENDGRID_API_KEY } = process.env;
+const { SENDGRID_API_KEY, EMAIL } = process.env;
 
 sgMeil.setApiKey(SENDGRID_API_KEY);
 
 const sendEmail = async (data) => {
-  const email = { ...data, from: "emihrant.ua@gmail.com" };
+  const email = { ...data, from: EMAIL };
   await sgMeil.send(email);
   return true;
 };
